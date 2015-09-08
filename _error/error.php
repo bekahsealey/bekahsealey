67f20f62df92ae35b5ed175335246ed60e151403
bekahsealey <bekah@bekahsealey.com>
2015-09-08 17:13:19 -0500

    switch to post.php script

--- _error/error.php
+++ _error/error.php
@@ -1,13 +1,3 @@
 <?php include('../inc/header.php'); ?>
-	<section class="col-2-3 center">
-		<header><h2>Lost?</h2></header>
-		<article>
-		<header><h3>Darn!</h3></header>
-			<p>You haven't found what you were looking for? &ldquo;Do what I do. Hold tight and pretend it's a plan!&rdquo; 
-&ndash;The Doctor</p>
-			<p><img class="tardis" src="/images/tardis.jpg"></p>
-			<p>You can hop in your tardis and head back to the <a href="/">home page</a>, or try the page navigation at the top.</p>
-	
-		</article>
-	</section>
+<?php writePage( 'error.txt' ); ?>
 <?php include('../inc/footer.php'); ?>
