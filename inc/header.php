ebe7fa166b35a054cd9ab1e27dc9f3ac308082a0
bekahsealey <bekah@bekahsealey.com>
2015-09-10 13:02:09 -0500

    update site header and logo

--- inc/header.php
+++ inc/header.php
@@ -7,27 +7,40 @@
 <html class="no-js">
 <head>
 <meta charset="UTF-8">
-<title>Bekah Sealey | Portfolio</title>
+<title><?php if ($page !== '') { echo ucwords( $page ), ' | '; } ?> Bekah Sealey | Portfolio</title>
 <link href="/css/screen.css" rel="stylesheet" media="screen">
-<link rel="stylesheet" href="/css/print.css" media="print">
+<link href="/css/print.css" rel="stylesheet" media="print">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script
+<link rel="icon" type="image/x-icon" href="//bekahsealey.com/favicon.ico"/>
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 </head>
 
 <body <?php if ($page != '') { ?>class="<?php echo $page; ?>"<?php } ?>>
 <div class="wrapper">
 <header>
 	<div class="grid">
+		<h1 class="logo col-1-2"><a href="/"><span class="icon icon-logo">&nbsp;</span>Bekah Sealey</a></h1>
 		<?php if ($page === 'resume') { ?>
-		<h1 class="hero col-1-2"><a href="/">Rebekah Sealey</a></h1>
 		<div class="col-1-2">
 			<h3>511 Frank Avenue, Algoma, WI  54201</h3>
-			<h4>(920) 487-2031 – <a href="mailto:rebekah@nmomedia.com">rebekah@nmomedia.com</a></h4>
+			<h4>(920) 487-2031 – <a href="mailto:bekah@bekahsealey.com">bekah@bekahsealey.com</a></h4>
 		</div>
 		<?php } else { ?>
-		<a href="/"><div class="hero col-1-3"><img src="/images/headshot.jpg"><h1>Bekah Sealey | Portfolio</h1></div></a>
-		<div class="col-2-3 reverse">
+		<div class="col-1-2 reverse">
 			<?php include('main-nav.php'); ?>
 			<?php include('social-nav.php'); ?>
 		</div>
