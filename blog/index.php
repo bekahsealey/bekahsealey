7f9237e351c774e81420f16c149da87ddfb8eaf2
bekahsealey <bekah@bekahsealey.com>
2015-09-24 15:52:27 -0500

    remove personal content

--- blog/index.php
+++ blog/index.php
@@ -1,10 +1,3 @@
 <?php include('../inc/header.php'); ?>
-	<section class="col-2-3 center">
-		<header><h2>Welcome!</h2></header>
-		<article>
-			<header><h3>I enjoy sharing.</h3></header>
-			<p>There is always something new to discover in the web development world. I collect information, resources, solutions to problems I've run into and other interesting things. There's no better person to share them with, than you!</p>
-		</article>
-	</section>
 <?php writePosts( 'txt', 5, true, true ); ?>
 <?php include('../inc/footer.php'); ?>
